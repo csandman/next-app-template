712cd8eb624700a66a9de75010db887807f160f2
Chris Sandvik <chris.sandvik@gmail.com>
2021-11-04 23:13:47 -0400

    Clean up prettier lint-staged script

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,6 +3,6 @@ module.exports = {
     `next lint --fix --file ${filenames
       .map((file) => file.split(process.cwd())[1])
       .join(' --file ')}`,
-  '*.{js,jsx,ts,tsx,css,less,scss,json,gql,md}': 'prettier --write',
+  '*': 'prettier --ignore-unknown --write',
   'package.json': 'prettier-package-json --write',
 };
